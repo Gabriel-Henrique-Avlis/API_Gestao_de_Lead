327104040ec6cd9fb9e376d8872ae962102e352b
Gabriel Henrique <gaabrielsilva.2011@gmail.com>
2021-10-26 00:30:53 -0300

    update)

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const PORT = 8383;
 
 app.use(express.json());
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Acces-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers","Content-Type")
+    res.header("Content-Type", "application/json");
+
+    next()
+})
+
 async function main() {
     await mongoose.connect('mongodb+srv://usuario_admin:c4SNdNmvziJXBJhf@cluster0.1xxzz.mongodb.net/Gestao_de_Leads?retryWrites=true');
 }
